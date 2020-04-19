1b33dddc1bc795a51f5d7d6056e497abde1e7500
Dhairya Surana <dhairs7@vt.edu>
2020-04-19 19:09:38 -0400

    Removed unnecessary functions

--- services.py
+++ services.py
@@ -1,3 +1,5 @@
+# Author: Dhairya Surana
+
 # services.py
 
 ##################### INITIALIZATION ######################
@@ -15,24 +17,8 @@ import sys
 import ServicesKeys
 import requests
 
-
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
 
 #################################### AUTHENTICATION ###########################################
 @auth.get_password
@@ -80,21 +66,7 @@ def getMarvelInfo():
 @app.route('/goodbye', methods=['GET'])
 @auth.login_required
 def goodbye():
-    
     return "Goodbye World"
-
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-@auth.login_required
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
 
 @app.errorhandler(404)
 def not_found(error):
@@ -109,4 +81,5 @@ if __name__ == '__main__':
         except Exception as e:
             print(e)
     else:
-        print("    Error: Incorrect args.\n    The format should be \"python3 services.py -p <WEBSERVER PORT>\"")
+        print("    Error: Incorrect args")
+        print("    The format should be \"python3 services.py -p <WEBSERVER PORT>\"")
